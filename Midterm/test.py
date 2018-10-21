bbc7d30d73e5ed052955b7298bc91338a9e6a1b3
VivekUdupa <vkoodli@clemson.edu>
2018-10-21 05:06:59 -0400

    random test

--- Midterm/test.py
+++ Midterm/test.py
@@ -1,1 +1,10 @@
-print("Hello World")
+
+array = []
+
+for i in range(10):
+	array.append(i)
+
+n = len(array) - 1
+
+print("last index is: ", n, "value is", array[n]) 
+print(array, "\t", end = "");
